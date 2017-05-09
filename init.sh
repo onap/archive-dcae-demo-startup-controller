b10781439dba48f4c39def83a34699a8343ceaa5
Carsten Lund <lund@research.att.com>
2017-05-09 20:07:50 +0000

    [UCA-10] support for non standard MTU
    Change-Id: I29a6d2358169a94d22b2611e077a9a095654fc93
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ U=$(grep NEXUS-USER /opt/app/dcae-controller/config.yaml | sed s/NEXUS-USER:.//)
 P=$(grep NEXUS-PASSWORD /opt/app/dcae-controller/config.yaml | sed s/NEXUS-PASSWORD:.//)
 D=$(grep DOCKER-REGISTRY /opt/app/dcae-controller/config.yaml | sed s/DOCKER-REGISTRY:.//)
 V=$(grep DCAE-VERSION /opt/app/dcae-controller/config.yaml | sed s/DCAE-VERSION:.//)
+MTU=$(/sbin/ifconfig | grep MTU | sed 's/.*MTU://' | sed 's/ .*//' | sort -n | head -1)
 
 if [ "$V" == "" ]; then V=1.0.0; fi
 
@@ -14,3 +15,4 @@ sed -i s/DCAE-VERSION/$V/ Makefile
 
 sed -i s/DOCKER-REGISTRY/$D/ docker-compose.yml
 sed -i s/DCAE-VERSION/$V/ docker-compose.yml
+sed -i s/MTU/$MTU/ docker-compose.yml
